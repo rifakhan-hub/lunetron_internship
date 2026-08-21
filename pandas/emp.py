c412fa9e4aa842a35d99c88c01502c2fc6dc89e0
rifakhan-hub <rifakhan3730@gmail.com>
2026-08-21 19:29:39 +0530

    updating pandas

--- pandas/emp.py
+++ pandas/emp.py
@@ -6,7 +6,8 @@ data = {
     "id" : [1, 2, 3, 4],
     "names" : ["raj", "aman", "riya", "oshi"],
     "class" : [5, 8, 10, 7],
-    "subject" : ["maths", "physics", "maths", "chemistry"]
+    "subject" : ["maths", "physics", "maths", "chemistry"],
+    "age": [20, 21, 22, 20]
 }
 
 df = pd.DataFrame(data)
@@ -19,5 +20,9 @@ print(df.columns)
 print(df["names"])
 print(df.iloc[0])
 # result = df[df["class"]>8]
-result = df[df["class"]<8]
+# result = df[df["age"]==22]
+
+result = df[(df["age"]>20) & (df["age"]<22)]
 print(result)
+
+print(df.describe())
