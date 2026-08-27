fb5c08c3db6e23ab63b422eb94ca63b137fd356e
rifakhan-hub <rifakhan3730@gmail.com>
2026-08-28 01:30:44 +0530

    data cleaning

---
+++ pandas/data_cleaning/map_1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv("Map-Scraper_20260826123513.csv")
+
+new_df = df[df["Emails"].str.contains("@gmail.com", na=False)]
+# Keep the entire row only if its Emails field contains a Gmail address
+
+new_df["Emails"] = new_df["Emails"].str.extract(r'([\w.-]+@gmail\.com)')
+print(new_df["Emails"])
+
+new_df = new_df[["Name", "Phone", "Emails"]]
+
+new_df = new_df.drop_duplicates()
+
+print(new_df)
